20df0d263837af713749633ec67477e9afdf333c
Jorge Queiroz <joqueiroz@deloitte.com>
2023-12-19 12:20:55 -0300

    First commit

---
+++ src/main/java/br/com/alurafood/jorge/dto/ItemDoPedidoDto.java
@@ -0,0 +1,14 @@
+package br.com.alurafood.jorge.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemDoPedidoDto {
+
+    private Long id;
+    private Integer quantidade;
+    private String descricao;
+}
---
+++ src/main/java/br/com/alurafood/jorge/dto/StatusPagamento.java
@@ -0,0 +1,8 @@
+package br.com.alurafood.jorge.dto;
+
+public enum StatusPagamento {
+    CRIADO,
+    CONFIRMADO,
+    CONFIRMADO_SEM_INTEGRACAO,
+    CANCELADO
+}
